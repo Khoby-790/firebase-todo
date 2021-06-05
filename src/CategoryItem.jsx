ecdfca33090df3430e58628cb251072bf00d7b3d
Khoby-790 <ebaidoo79@gmail.com>
2021-06-05 23:47:56 +0000

    Modified src/CategoryItem.jsx

--- src/CategoryItem.jsx
+++ src/CategoryItem.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaHome } from "react-icons/fa";
+import { categoryIcons } from "./categories";
 
 const CategoryItem = ({ icon, text }) => {
   return (
     <div className="bg-white flex items-center p-3 hover:shadow-lg hover:bg-gray-900 hover:text-white cursor-pointer hover:rounded-lg">
-      <FaHome />
+      {categoryIcons[text]}
       <span className="text-lg ml-3">{text}</span>
     </div>
   );
