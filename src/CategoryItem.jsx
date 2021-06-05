6811701482312f8f5a6ea266cbc92bbdf61e58c6
Khoby-790 <ebaidoo79@gmail.com>
2021-06-05 23:57:57 +0000

    Modified src/CategoryItem.jsx,Modified src/TodoForm.jsx

--- src/CategoryItem.jsx
+++ src/CategoryItem.jsx
@@ -7,7 +7,9 @@ const CategoryItem = ({ text, onClick, active }) => {
       onClick={onClick ? () => onClick(text) : () => {}}
       className={`${
         active ? "bg-gray-900 text-white" : "bg-white text-gray-900"
-      } flex text-2xl items-center p-3 hover:shadow-lg  hover:text-white cursor-pointer hover:rounded-lg`}
+      } flex text-2xl items-center p-3 hover:shadow-lg  ${
+        active ? "hover:text-gray-900" : "hover:text-white"
+      } cursor-pointer hover:rounded-lg`}
     >
       {categoryIcons[text]}
       <span className="text-lg ml-3">{text}</span>
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { categories } from "./categories";
 import CategoryItem from "./CategoryItem";
 
 const TodoForm = () => {
+  const [_categories, setCategories] = useState([]);
   return (
     <div className="w-1/3 bg-gradient-to-b from-bg-2  to-bg-fta-primary rounded-xl py-5 px-3">
       <form className="flex flex-col justify-center mx-4 flex-1 h-full">
@@ -21,6 +23,9 @@ const TodoForm = () => {
           </div>
 
           <div className="grid grid-cols-2 gap-3">
+            {categories.map((cat, id) => (
+              <CategoryItem key={id} text={cat.text} />
+            ))}
             <CategoryItem text="Home" />
             <CategoryItem text="Finance" />
             <CategoryItem text="Work" />
