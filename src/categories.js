c68e4bd75348b568609e2a9e0fac6ca80e7546d3
Khoby-790 <ebaidoo79@gmail.com>
2021-06-05 23:45:56 +0000

    Modified src/categories.js

---
+++ src/categories.js
@@ -0,0 +1,23 @@
+import { FaBuilding, FaHome } from "react-icons/fa"
+
+
+
+export const categories = [
+    {
+        text: "Home"
+    },
+    {
+        text: "Work"
+    },
+    {
+        text: "Finance"
+    },
+    {
+        text: "Music"
+    }
+]
+
+export const categoryIcons = {
+    "Home": <FaHome />,
+    "Work": <FaBuilding />
+}
