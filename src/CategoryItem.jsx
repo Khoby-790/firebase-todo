8236b4393c58b8ebb38661c19f24c86fee4cdae7
Khoby-790 <ebaidoo79@gmail.com>
2021-06-05 23:55:56 +0000

    Modified src/CategoryItem.jsx

--- src/CategoryItem.jsx
+++ src/CategoryItem.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { categoryIcons } from "./categories";
 
-const CategoryItem = ({ text }) => {
+const CategoryItem = ({ text, onClick, active }) => {
   return (
-    <div className="bg-white flex text-2xl items-center p-3 hover:shadow-lg hover:bg-gray-900 hover:text-white cursor-pointer hover:rounded-lg">
+    <div
+      onClick={onClick ? () => onClick(text) : () => {}}
+      className={`${
+        active ? "bg-gray-900 text-white" : "bg-white text-gray-900"
+      } flex text-2xl items-center p-3 hover:shadow-lg  hover:text-white cursor-pointer hover:rounded-lg`}
+    >
       {categoryIcons[text]}
       <span className="text-lg ml-3">{text}</span>
     </div>
