1e13e14e23812517ec582f1745d03803231539cb
Khoby-790 <ebaidoo79@gmail.com>
2021-06-06 01:23:04 +0000

    Modified src/TodoListing.jsx

--- src/TodoListing.jsx
+++ src/TodoListing.jsx
@@ -25,7 +25,7 @@ const TodoListing = () => {
       </div>
       <div className="grid mt-4 grid-cols-3 gap-3">
         {todos.map((todo, todoId) => (
-          <div key={todoId} className="bg-gray-500 cursor-pointer p-2">
+          <div key={todoId} className="bg-bg-2 cursor-pointer p-2">
             <div className="text-xl flex py-2 flex-row">
               {todo.categories.map((cat, catId) => (
                 <span className="mx-2">{categoryIcons[cat]}</span>
