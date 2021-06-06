a6e1949a4e22e4633b1f59297776c3f0df2bd1b9
Khoby-790 <ebaidoo79@gmail.com>
2021-06-06 00:58:03 +0000

    Modified src/TodoListing.jsx

--- src/TodoListing.jsx
+++ src/TodoListing.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import fire from "./fire";
 
 const TodoListing = () => {
-  return <div className="w-auto flex-1 bg-fta-primary"></div>;
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    let messageRef = fire
+      .database()
+      .ref("fta-todos")
+      .orderByKey()
+      .limitToLast(100);
+
+    messageRef.on("child_added", (snapshot) => {
+      let todo = { ...snapshot.val(), id: snapshot.key };
+      setTodos((prev) => [...prev, todo]);
+    });
+  }, []);
+
+  return (
+    <div className="w-auto flex-1 bg-fta-primary">{JSON.stringify(todos)}</div>
+  );
 };
 
 export default TodoListing;
