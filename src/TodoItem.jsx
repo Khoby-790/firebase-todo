5435d72d6ec15e07cf11c60015d9d47bfeb40e3f
Khoby-790 <ebaidoo79@gmail.com>
2021-06-13 12:29:00 +0000

    Modified src/TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,33 +1,30 @@
 import React from "react";
 import { categoryIcons } from "./categories";
-import { Spring } from "react-spring";
+import { useSpring } from "react-spring";
 
 const TodoItem = ({ todo = {} }) => {
+  const props = useSpring({
+    from: {
+      opacity: 0,
+      marginTop: -1000,
+    },
+    to: { opacity: 1, marginBottom: 0 },
+  });
+
   return (
-    <Spring
-      from={{
-        opacity: 0,
-        marginTop: -1000,
-      }}
-      to={{ opacity: 1, marginBottom: 0 }}
-    >
-      {(props) => (
-        <div
-          style={props}
-          className=" bg-gray-500 shadow-xl  cursor-pointer p-2"
-        >
-          <div className="text-xl flex py-2 flex-row">
-            {todo.categories.map((cat, catId) => (
-              <span key={catId} className="mx-2">
-                {categoryIcons[cat]}
-              </span>
-            ))}
-          </div>
-          <span className="font-semibold text-2xl">{todo.title}</span>
-          <p className="font-light truncate">{todo.description}</p>
+    <>
+      <div style={props} className=" bg-gray-500 shadow-xl  cursor-pointer p-2">
+        <div className="text-xl flex py-2 flex-row">
+          {todo.categories.map((cat, catId) => (
+            <span key={catId} className="mx-2">
+              {categoryIcons[cat]}
+            </span>
+          ))}
         </div>
-      )}
-    </Spring>
+        <span className="font-semibold text-2xl">{todo.title}</span>
+        <p className="font-light truncate">{todo.description}</p>
+      </div>
+    </>
   );
 };
 
