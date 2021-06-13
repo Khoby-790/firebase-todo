6844c72f7568461381889212d1c46aeac335d63a
Khoby-790 <ebaidoo79@gmail.com>
2021-06-13 12:00:01 +0000

    Modified src/TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -4,7 +4,7 @@ import { categoryIcons } from "./categories";
 const TodoItem = ({ todo = {} }) => {
   return (
     <>
-      <div className="bg-gradient-to-b from-bg-2 via-gray-300 shadow-xl to-bg-fta-primary cursor-pointer p-2">
+      <div className="bg-gradient-to-b from-bg-2 via-gray-300 shadow-xl  cursor-pointer p-2">
         <div className="text-xl flex py-2 flex-row">
           {todo.categories.map((cat, catId) => (
             <span key={catId} className="mx-2">
