83f373e512c64f9c467f9c69185667308ad4562e
Khoby-790 <ebaidoo79@gmail.com>
2021-06-13 19:34:44 +0000

    Modified src/TodoListing.jsx

--- src/TodoListing.jsx
+++ src/TodoListing.jsx
@@ -2,8 +2,23 @@ import React, { useEffect, useState } from "react";
 import { AtomSpinner } from "react-epic-spinners";
 import fire from "./fire";
 import TodoItem from "./TodoItem";
+import { useSprings, animated, interpolate } from "react-spring";
+
+const to = (i) => ({
+  x: 0,
+  y: i * -4,
+  scale: 1,
+  rot: -10 + Math.random() * 20,
+  delay: i * 100,
+});
+const from = (i) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
+
 const TodoListing = () => {
   const [todos, setTodos] = useState([]);
+  const [props, set] = useSprings(todos.length, (i) => ({
+    ...to(i),
+    from: from(i),
+  })); // Create a bunch of springs using the helpers above
 
   useEffect(() => {
     const auth = localStorage.getItem("fta-auth");
@@ -19,11 +34,30 @@ const TodoListing = () => {
     });
   }, []);
 
+  const trans = (r, s) =>
+    `perspective(1500px) rotateX(30deg) rotateY(${
+      r / 10
+    }deg) rotateZ(${r}deg) scale(${s})`;
+
   return (
     <div className="w-auto flex-1 flex flex-col text-white px-5 py-3 bg-fta-primary">
       <div className="">
         <span className="text-3xl font-thin">Todos</span>
       </div>
+      {props.map(({ x, y, rot, scale }, i) => (
+        <animated.div
+          key={i}
+          style={{
+            transform: interpolate(
+              [x, y],
+              (x, y) => `translate3d(${x}px,${y}px,0)`
+            ),
+          }}
+        >
+          {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+          <TodoItem todo={todos[i]} />
+        </animated.div>
+      ))}
       {todos.length < 1 && (
         <div className="text-white w-full h-full flex justify-center items-center">
           <AtomSpinner color="red" />
