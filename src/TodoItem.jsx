7c6f1b81f82a8c45bf7d77543b966617cbd05ce3
Khoby-790 <ebaidoo79@gmail.com>
2021-06-13 14:11:02 +0000

    Modified src/TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -14,17 +14,19 @@ const TodoItem = ({ todo = {} }) => {
   return (
     <animated.div
       style={props}
-      className=" bg-gray-500 shadow-xl hover:bg-gray-700 duration-300 transform transition-all rounded-none cursor-pointer p-2"
+      className=" bg-gray-500 shadow-xl group hover:bg-gray-700 duration-300 transform transition-all rounded-none cursor-pointer p-2"
     >
-      <div className="text-xl flex py-2 flex-row">
-        {todo.categories.map((cat, catId) => (
-          <span key={catId} className="mx-2">
-            {categoryIcons[cat]}
-          </span>
-        ))}
+      <div className="w-auto">
+        <div className="text-xl flex  py-2 flex-row">
+          {todo.categories.map((cat, catId) => (
+            <span key={catId} className="mx-2">
+              {categoryIcons[cat]}
+            </span>
+          ))}
+        </div>
+        <span className="font-semibold text-2xl">{todo.title}</span>
+        <p className="font-light truncate">{todo.description}</p>
       </div>
-      <span className="font-semibold text-2xl">{todo.title}</span>
-      <p className="font-light truncate">{todo.description}</p>
     </animated.div>
   );
 };
